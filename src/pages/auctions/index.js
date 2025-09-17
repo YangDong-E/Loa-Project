f517d66e8edb4811a7f104db1c5cd6c0ccdec1d9
YangDong-E <ehddh125@gmail.com>
2025-09-17 18:59:16 +0900

    LostArkProject(로스트아크 API 변경으로 인한 코드 변경 - 진행중(반응형 scss 코드 적용중))

--- src/pages/auctions/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Auction from '../../components/auctions/auction'
-import '../../assets/scss/auctions.scss'
-
-const Auctions = () => {
-    return <Auction />
-}
-
-export default Auctions
--- src/utils/index.js
+++
@@ -1,4 +0,0 @@
-import { imageList } from './imageList'
-
-export const findImage = (name) =>
-    imageList.find((image) => image.name === name).src
